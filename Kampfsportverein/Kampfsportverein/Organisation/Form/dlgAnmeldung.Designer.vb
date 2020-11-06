f2e157b78d010d422e6bff048cdb1abd63d8cab9
mesyamaureen <mesyamaureen@gmail.com>
2020-11-06 18:14:00 +0100

    dlgAnmeldung erstellt

---
+++ Kampfsportverein/Kampfsportverein/Organisation/Form/dlgAnmeldung.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class dlgAnmeldung
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtBenutzername = New System.Windows.Forms.TextBox()
+        Me.txtPasswort = New System.Windows.Forms.TextBox()
+        Me.lblBenutzername = New System.Windows.Forms.Label()
+        Me.lblPasswort = New System.Windows.Forms.Label()
+        Me.btnAbbrechen = New System.Windows.Forms.Button()
+        Me.btnAnmelden = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtBenutzername
+        '
+        Me.txtBenutzername.Location = New System.Drawing.Point(173, 47)
+        Me.txtBenutzername.Name = "txtBenutzername"
+        Me.txtBenutzername.Size = New System.Drawing.Size(597, 31)
+        Me.txtBenutzername.TabIndex = 0
+        '
+        'txtPasswort
+        '
+        Me.txtPasswort.Location = New System.Drawing.Point(182, 134)
+        Me.txtPasswort.Name = "txtPasswort"
+        Me.txtPasswort.Size = New System.Drawing.Size(588, 31)
+        Me.txtPasswort.TabIndex = 1
+        '
+        'lblBenutzername
+        '
+        Me.lblBenutzername.AutoSize = True
+        Me.lblBenutzername.Location = New System.Drawing.Point(24, 53)
+        Me.lblBenutzername.Name = "lblBenutzername"
+        Me.lblBenutzername.Size = New System.Drawing.Size(302, 50)
+        Me.lblBenutzername.TabIndex = 2
+        Me.lblBenutzername.Text = "Benutzername"
+        '
+        'lblPasswort
+        '
+        Me.lblPasswort.AutoSize = True
+        Me.lblPasswort.Location = New System.Drawing.Point(24, 137)
+        Me.lblPasswort.Name = "lblPasswort"
+        Me.lblPasswort.Size = New System.Drawing.Size(200, 50)
+        Me.lblPasswort.TabIndex = 3
+        Me.lblPasswort.Text = "Passwort"
+        '
+        'btnAbbrechen
+        '
+        Me.btnAbbrechen.Location = New System.Drawing.Point(638, 389)
+        Me.btnAbbrechen.Name = "btnAbbrechen"
+        Me.btnAbbrechen.Size = New System.Drawing.Size(132, 36)
+        Me.btnAbbrechen.TabIndex = 4
+        Me.btnAbbrechen.Text = "Abbrechen"
+        Me.btnAbbrechen.UseVisualStyleBackColor = True
+        '
+        'btnAnmelden
+        '
+        Me.btnAnmelden.Location = New System.Drawing.Point(479, 389)
+        Me.btnAnmelden.Name = "btnAnmelden"
+        Me.btnAnmelden.Size = New System.Drawing.Size(132, 36)
+        Me.btnAnmelden.TabIndex = 6
+        Me.btnAnmelden.Text = "Anmelden"
+        Me.btnAnmelden.UseVisualStyleBackColor = True
+        '
+        'dlgAnmeldung
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnAnmelden)
+        Me.Controls.Add(Me.btnAbbrechen)
+        Me.Controls.Add(Me.lblPasswort)
+        Me.Controls.Add(Me.lblBenutzername)
+        Me.Controls.Add(Me.txtPasswort)
+        Me.Controls.Add(Me.txtBenutzername)
+        Me.Name = "dlgAnmeldung"
+        Me.Text = "Anmeldung"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtBenutzername As TextBox
+    Friend WithEvents txtPasswort As TextBox
+    Friend WithEvents lblBenutzername As Label
+    Friend WithEvents lblPasswort As Label
+    Friend WithEvents btnAbbrechen As Button
+    Friend WithEvents btnAnmelden As Button
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblPasswort.Click
+
+    End Sub
+End Class
