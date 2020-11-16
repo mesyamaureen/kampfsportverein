468692d0f51669d0a263c889e276f5c4c0c4cefc
mesyamaureen <mesyamaureen@gmail.com>
2020-11-17 00:45:14 +0100

    Attribute; Felder und Eigenschaften der Klasse Benutzer hinzugefügt

--- Kampfsportverein/Kampfsportverein/Organisation/Model/Benutzer.vb
+++ Kampfsportverein/Kampfsportverein/Organisation/Model/Benutzer.vb
@@ -1,3 +1,47 @@
 ﻿Public Class Benutzer
+    Private mstrVorname As String
+    Private mstrName As String
+    Private mstrBenutzername As String
+    Private mstrPasswort As String
+    Private mintBenutzerID As Integer
 
+    Public Property BenutzerID As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property Benutzername As String
+        Get
+            Return Nothing
+        End Get
+        Set(value As String)
+        End Set
+    End Property
+
+    Public Property Name As String
+        Get
+            Return Nothing
+        End Get
+        Set(value As String)
+        End Set
+    End Property
+
+    Public Property Passwort As String
+        Get
+            Return Nothing
+        End Get
+        Set(value As String)
+        End Set
+    End Property
+
+    Public Property Vorname As String
+        Get
+            Return Nothing
+        End Get
+        Set(value As String)
+        End Set
+    End Property
 End Class
