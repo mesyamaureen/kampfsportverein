1f462c241ae024374c08d28fbe182840b1bad5e6
Hochtritt <hochtritt@berlin.strato.de>
2020-11-18 15:37:38 +0100

    Anpassen von MeinTrainerKontoView & Trainer

--- Kampfsportverein/Kampfsportverein/Organisation/Model/Trainer.vb
+++ Kampfsportverein/Kampfsportverein/Organisation/Model/Trainer.vb
@@ -14,10 +14,6 @@
 
     End Sub
 
-    '  Public Shared Widening Operator CType(v As (String, String, String, String, Integer)) As Trainer
-    ' Throw New NotImplementedException()
-    ' End Operator
-
     Public Property Schueler As Schueler
         Get
             Return Nothing
--- Kampfsportverein/Kampfsportverein/Organisation/View/MeinTrainerkontoView.vb
+++ Kampfsportverein/Kampfsportverein/Organisation/View/MeinTrainerkontoView.vb
@@ -2,23 +2,18 @@
     Inherits dlgMeinKontoTrainer
 
     Sub New(pPresenter As TrainerkontoPresenter)
-
         MyBase.New
         mPresenter = pPresenter
-
     End Sub
 
     Public Sub leeren()
-
         Me.txtVorname.Text = String.Empty
         Me.txtName.Text = String.Empty
         Me.txtBenutzername.Text = String.Empty
         Me.txtPasswort.Text = String.Empty
-
     End Sub
 
     Public Sub anzeigen(pintBenutzerID As Integer, pstrVorname As String, pstrName As String, pstrBenutzername As String, pstrPasswort As String)
-
         Me.txtTrainerID.Enabled = False
         Me.txtVorname.Enabled = True
         Me.txtName.Enabled = True
@@ -26,8 +21,10 @@
         Me.txtPasswort.Enabled = True
 
         Me.txtTrainerID.Text = pintBenutzerID
-
-
+        Me.txtVorname.Text = pstrVorname
+        Me.txtName.Text = pstrName
+        Me.txtBenutzername.Text = pstrBenutzername
+        Me.txtPasswort.Text = pstrPasswort
     End Sub
 
 End Class
