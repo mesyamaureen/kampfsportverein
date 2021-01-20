2cda51d7a9a9711230936c254ffb6eaa19d6495f
mesyamaureen <mesyamaureen@gmail.com>
2021-01-21 00:18:57 +0100

    Verbindung zur Datenbank mit ConnectionString erstellt

--- Kampfsportverein/Kampfsportvereinverwaltung/Program.vb
+++ Kampfsportverein/Kampfsportvereinverwaltung/Program.vb
@@ -1,7 +1,33 @@
-Imports System
+Imports System.Data.OleDb
 
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+'Module Program
+'Sub Main(args As String())
+'Console.WriteLine("Hello World!")
+'End Sub
+'End Module
+
+Module Datenbank
+    'auf die Datenbank zugreifen
+    Dim conn As OleDbConnection
+
+    Public Sub Main()
+        oeffnenDbVerbindung()
+        schlieﬂenDbVerbindung()
+    End Sub
+
+    'Oeffnen der Datenbankverbindung
+    Private Sub oeffnenDbVerbindung()
+        Dim strConnectionString As String
+        strConnectionString = My.Settings.ConnectionString
+
+        conn = New OleDbConnection(strConnectionString)
+        conn.Open()
+    End Sub
+
+    'Schlieﬂen der Datenbankverbindung
+    Private Sub schlieﬂenDbVerbindung()
+        If Not (conn Is Nothing) Then
+            conn.Close()
+        End If
     End Sub
 End Module
